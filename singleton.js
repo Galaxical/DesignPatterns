5e38870de36104f592f4d28f38e2ed9b48c5a87c
azisomtochukwu@gmail.com <azisomtochukwu@gmail.com>
2024-03-03 06:37:19 +0100

    dialPhone, ObserverAdd,Notify

--- singleton.js
+++ singleton.js
@@ -17,4 +17,21 @@ class phone {
             this.phoneNumbers.splice(index, 1);
         }
     }
+
+    dialPhoneNumber(phoneNumber){
+        if(this.phoneNumbers.include(phoneNumber)){
+            console.log(`Dialing ${phoneNumber}`);
+            this.notifyObservers(phoneNumber)
+        }else{
+            console.log(`${phoneNumber} is not a registered number`)
+        }
+    }
+
+    addObserver(observers){
+        this.observers.push(observers);
+    }
+
+    notifyObservers(phoneNumber){
+        this.observers.forEach((observer) => observer.callLog(phoneNumber));
+    }
 }
